3f9548681b747c1e444f027435b789ddbb1c4611
Carrick FB <carrickfb@gmail.com>
2023-12-11 22:04:44 +0000

    Install Storybook, Install ESLint, Configure Aliases

---
+++ craco.config.ts
@@ -0,0 +1,10 @@
+import path from 'path';
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components')
+    }
+  }
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import "./index.css";
-import { App } from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
-
+import './index.css';
+import { App } from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode!);
