a698efb3c2b008a61bc96255c3727f5e9b3afbf0
Carrick FB <carrickfb@gmail.com>
2023-12-11 22:30:13 +0000

    Add basic Layout Component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import logo from "../logo.svg";
-import styles from "./App.module.scss";
+import React from 'react';
+import logo from '@src/logo.svg';
+import styles from './App.module.scss';
+import { Layout } from '@components';
 
 // Character list is available in the public directory
 
 export function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles["App-header"]}>
-        <img src={logo} className={styles["App-logo"]} alt="logo" />
+    <div className={styles['App']}>
+      <header className={styles['App-header']}>
+        <img src={logo} className={styles['App-logo']} alt="logo" />
         <h1 className="App-title">Lord of the Rings Character Index</h1>
       </header>
 
       <section className="App-content">
-        {/* Lovely character list goes here */}
+        <Layout></Layout>
       </section>
     </div>
   );
